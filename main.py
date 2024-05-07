ecf1f46dd0ea0b6bde8d6473c1453dc4185e34d1
W Isnal <wanda.isnaldi@yahoo.com>
2024-05-07 16:04:34 +0700

    training added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Classification import logger
 from Classification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Classification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from Classification.pipeline.stage_03_training import TrainingModelStagePipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -19,6 +20,18 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     prepare_base_model = PrepareBaseModelTrainingPipeline()
     prepare_base_model.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nx=========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Training Model Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = TrainingModelStagePipeline()
+    obj.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nx=========x")
 except Exception as e:
     logger.exception(e)
--- src/Classification/config/configuration.py
+++ src/Classification/config/configuration.py
@@ -1,7 +1,7 @@
 import os
 from Classification.constants import *
 from Classification.utils.common import read_yaml, create_directories
-from Classification.entity.config_entity import DataIngestionConfig,PrepareBaseModelConfig,PrepareCallbacksConfig
+from Classification.entity.config_entity import DataIngestionConfig,PrepareBaseModelConfig,PrepareCallbacksConfig,TrainingConfig
 
 
 class ConfigurationManager:
@@ -25,7 +25,7 @@ class ConfigurationManager:
         data_ingestion_config = DataIngestionConfig(
             root_dir=config.root_dir,
             local_data_file=config.local_data_file,
-            unzip_dir=config.unzip_dir 
+            unzip_dir=config.unzip_dir
         )
 
         return data_ingestion_config
@@ -65,3 +65,25 @@ class ConfigurationManager:
         )
 
         return prepare_callbacks_config
+    
+
+
+    def get_training_config(self) -> TrainingConfig:
+        training = self.config.training
+        prepare_base_model = self.config.prepare_base_model
+        params = self.params
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir,"Potato-dieses")
+        create_directories([Path(training.root_dir)])
+
+        training_config = TrainingConfig(
+            root_dir=Path(training.root_dir),
+            trained_model_path=Path(training.trained_model_path),
+            updated_base_model_path=Path(prepare_base_model.update_base_model_path),
+            training_data=Path(training_data),
+            params_epochs=params.EPOCHS,
+            params_batch_size=params.BATCH_SIZE,
+            params_is_augmentation=params.AUGMENTATION,
+            params_image_size=params.IMAGE_SIZE
+        )
+
+        return training_config
